f9a9680e6fe8a180219df343f11c0e73a020daa6
lkusters <lienekekusters@gmail.com>
2016-11-18 14:27:07 +0100

    Support non-zipped files in import sequences

--- DNAseqhandling/seq_import.py
+++ DNAseqhandling/seq_import.py
@@ -17,6 +17,7 @@ def seqgenerator(filenames_list):
     and return a generator of sequences (without identifiers)
     """
     cur_version = sys.version_info
+        
 
     if cur_version.major == 2:
         # python version is 2, note that I don't expect this to happen!
@@ -24,35 +25,46 @@ def seqgenerator(filenames_list):
                       .format(cur_version)
                       )
         for filename in filenames_list:
-            handle = gzip.open(filename, 'r')
-
+            # first check if it is zipped!
             checkextension = filename.split('.')
-            if checkextension[-2] == 'fna' or checkextension[-2] == 'fa':
+            if checkextension[-1] == 'gz':
+                handle = gzip.open(filename, 'r')
+                checkextension.pop(-1)
+            else:
+                handle = open(filename, 'r')
+                
+            if checkextension[-1] == 'fna' or checkextension[-1] == 'fa':
                 for record in SeqIO.parse(handle, 'fasta'):
                     yield str(record.seq)
-            elif checkextension[-2] == 'fastq':
+            elif checkextension[-1] == 'fastq':
                 for record in SeqIO.parse(handle, 'fastq'):
                     yield str(record.seq)
             else:
                 raise ValueError("filename extension {0} not recognised"
-                                 .format(checkextension[-2]))
+                                 .format(checkextension[-1]))
 
     elif cur_version.major == 3:
         for filename in filenames_list:
-            handle = gzip.open(filename, 'rt')
+            # first check if it is zipped!
+            checkextension = filename.split('.')
+            if checkextension[-1] == 'gz':
+                handle = gzip.open(filename, 'rt')
+                checkextension.pop(-1)
+            else:
+                handle = open(filename, 'rt')
 
             checkextension = filename.split('.')
-            if checkextension[-2] == 'fna' or checkextension[-2] == 'fa':
+            if checkextension[-1] == 'fna' or checkextension[-1] == 'fa':
                 for record in SeqIO.parse(handle, 'fasta'):
                     sys.stderr.write(str(record.description)+'\n')
                     yield str(record.seq)
-            elif checkextension[-2] == 'fastq':
+            elif checkextension[-1] == 'fastq':
                 for record in SeqIO.parse(handle, 'fastq'):
                     sys.stderr.write(str(record.description)+'\n')
                     yield str(record.seq)
             else:
                 raise ValueError("filename extension {0} not recognised"
-                                 .format(checkextension[-2]))
+                                 .format(checkextension[-1]))
 
     else:
         raise EnvironmentError("python version {0} incompatible with code"
